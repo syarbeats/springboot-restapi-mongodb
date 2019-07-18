dcd8eca7c9eb05b2670cd1a86788f2f204d8ef75
Syarif Hidayat <Syarif_H657@mitrais.com>
2019-07-18 13:47:06 +0700

    change server endpoint

--- src/main/java/com/mitrais/cdc/mongodbapp/controller/CrossOriginController.java
+++ src/main/java/com/mitrais/cdc/mongodbapp/controller/CrossOriginController.java
@@ -2,6 +2,6 @@ package com.mitrais.cdc.mongodbapp.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins="http://128.199.158.139:3000")
+@CrossOrigin(origins="http://localhost:3000")
 public class CrossOriginController {
 }
